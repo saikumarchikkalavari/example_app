907738aa2906ecedec4c209ee8eec894a9a256ba
Sai Kumar <chisaikumar@deloitte.com>
2026-01-15 09:33:52 +0530

    changes done

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
-  ],
   testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$'
-  ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
  
