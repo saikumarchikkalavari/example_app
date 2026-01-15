f9a6dae08c913da0647bc960dca81900ed9e71e5
Sai Kumar <chisaikumar@deloitte.com>
2026-01-15 08:56:18 +0530

    changes done

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,19 @@ module.exports = {
     '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)'
   ],
-  testPathIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/build/'
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
 };
+ 
